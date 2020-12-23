e598c5a288a6d531a733fbf7986a03eaa4499d42
Simon-A-P <simon.pfaus@web.de>
2020-12-23 23:22:17 +0100

    Change Rust 'Gigasecond' solution according to mentor feedback

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
 use chrono::{DateTime, Duration, Utc};
 
+const GIGASECOND: i64 = 1_000_000_000;
+
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(1_000_000_000)
+    start + Duration::seconds(GIGASECOND)
 }
