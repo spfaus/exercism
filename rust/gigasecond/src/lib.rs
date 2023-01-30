cfa5d73f71a87cbd44d6073f25a573d6d9e3bc7c
spfaus <simon.pfaus@web.de>
2023-01-30 14:55:28 -0500

    Remove exercises

--- rust/gigasecond/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use chrono::{DateTime, Duration, Utc};
-
-const GIGASECOND: i64 = 1_000_000_000;
-
-// Returns a Utc DateTime one billion seconds after start.
-pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(GIGASECOND)
-}
--- rust/reverse-string/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// [dependencies]
-// unicode-segmentation = "1.7.1"
-
-use unicode_segmentation::UnicodeSegmentation;
-
-pub fn reverse(input: &str) -> String {
-    input.graphemes(true).rev().collect()
-}
